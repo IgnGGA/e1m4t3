75ff8b9e18720514a1a0cae1a7a69f92457b5ab2
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-11 11:16:03 -0300

    se elimina codigo comentado; se vinculan tablas a la vista info, en vez de que estas este directamente en la vista

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -27,118 +27,10 @@
         <div id="infotable">
 
          <div v-if="showS">
-            <table class="table table-bordered">
-                    <tr>
-                      <th colspan="4" id="sept">SEPTEMBER</th>
-                    <tr>
-                    <th>Date</th>
-                    <th>Teams</th>
-                    <th>Location</th>
-                    <th>Times</th>
-                    </tr>
-                    <tr>
-                      <td rowspan="2">9/01</td>
-                      <td v-if="u1 && u4"> <img src="../assets/icon/G1.png" alt="icono" class="icono">U1 and U4<img src="../assets/icon/G4.png" alt="icono" class="icono"></td>
-                      <td v-if="u1 && u4">AJ Katzenmaier</td>
-                      <td v-if="u1 && u4">9:30 a.m.</td>
-                    </tr>
-                    <tr>
-                    <td><img src="../assets/icon/G3.png" alt="icono" class="icono">U3 and U2<img src="../assets/icon/G2.png" alt="icono" class="icono"></td>
-                    <td>Greenbay</td>
-                    <td>1:00 p.m.</td>
-                    </tr>
-                    <tr>
-                    <td rowspan="2">9/08</td>
-                    <td><img src="../assets/icon/G5.png" alt="icono" class="icono">U5 and U6<img src="../assets/icon/G6.png" alt="icono" class="icono"></td>
-                    <td>Howard A Yeager</td>
-                    <td>9:30 a.m.</td>
-                    </tr>
-                    <tr>
-                    <td><img src="../assets/icon/G6.png" alt="icono" class="icono">U6 and U1<img src="../assets/icon/G1.png" alt="icono" class="icono"></td>
-                    <td>Marjorie P Hart</td>
-                    <td>1:00 p.m.</td>
-                    <tr>
-                    <td rowspan="2">9/15</td>
-                    <td><img src="../assets/icon/G2.png" alt="icono" class="icono">U2 and U4<img src="../assets/icon/G4.png" alt="icono" class="icono"></td>
-                    <td>North</td>
-                    <td>9:30 a.m.</td>
-                    </tr>
-                    <tr>
-                    <td><img src="../assets/icon/G3.png" alt="icono" class="icono">U3 and U5<img src="../assets/icon/G5.png" alt="icono" class="icono"></td>
-                    <td>AJ Katzenmaier</td>
-                    <td>1:00 p.m.</td>
-                    <tr>
-                    <td rowspan="2">9/22</td>
-                    <td><img src="../assets/icon/G1.png" alt="icono" class="icono">U1 and U3<img src="../assets/icon/G3.png" alt="icono" class="icono"></td>
-                    <td>South</td>
-                    <td>9:30 a.m.</td>
-                    </tr>
-                    <tr>
-                    <td><img src="../assets/icon/G2.png" alt="icono" class="icono">U2 and U6<img src="../assets/icon/G6.png" alt="icono" class="icono"></td>
-                    <td>Howard A Yeager</td>
-                    <td>1:00 p.m.</td>
-                    <tr>
-                    <td>9/29</td>
-                    <td><img src="../assets/icon/G4.png" alt="icono" class="icono">U4 and U5<img src="../assets/icon/G5.png" alt="icono" class="icono"></td>
-                    <td>Greenbay</td>
-                    <td>9:30 a.m.</td>
-                    </tr>
-              </table>
+            <TablaSep></TablaSep>
         </div>
         <div  v-if="showO">
-            <table class="table table-bordered">
-              <tr>
-                  <th colspan="4" id="oct">OCTOBER</th>
-              <tr>
-            <tr>
-              <th>Date</th>
-              <th>Teams</th>
-              <th>Location</th>
-              <th>Times</th>
-            </tr>
-            <tr>
-              <td rowspan="2">10/06</td>
-              <td><img src="../assets/icon/G2.png" alt="icono" class="icono">U2 and U5<img src="../assets/icon/G5.png" alt="icono" class="icono"></td>
-              <td>Marjorie P Hart</td>
-              <td>9:30 a.m.</td>
-            </tr>
-            <tr>
-              <td><img src="../assets/icon/G1.png" alt="icono" class="icono">U1 and U6<img src="../assets/icon/G6.png" alt="icono" class="icono"></td>
-              <td>South</td>
-              <td>1:00 p.m.</td>
-            </tr>
-            <tr>
-              <td rowspan="2">10/08</td>
-              <td><img src="../assets/icon/G3.png" alt="icono" class="icono">U3 and U4<img src="../assets/icon/G4.png" alt="icono" class="icono"></td>
-              <td>Howard A Yeager</td>
-              <td>9:30 a.m.</td>
-            </tr>
-            <tr>
-              <td><img src="../assets/icon/G5.png" alt="icono" class="icono">U5 and U1<img src="../assets/icon/G1.png" alt="icono" class="icono"></td>
-              <td>Greenbay</td>
-              <td>1:00 p.m.</td>
-          <tr>
-              <td rowspan="2">10/20</td>
-              <td><img src="../assets/icon/G6.png" alt="icono" class="icono">U6 and U3<img src="../assets/icon/G3.png" alt="icono" class="icono"></td>
-              <td>North</td>
-              <td>9:30 a.m.</td>
-            </tr>
-            <tr>
-              <td><img src="../assets/icon/G2.png" alt="icono" class="icono">U2 and U4<img src="../assets/icon/G4.png" alt="icono" class="icono"></td>
-              <td>Marjorie P Hart</td>
-              <td>1:00 p.m.</td>
-            <tr>
-              <td rowspan="2">10/27</td>
-              <td><img src="../assets/icon/G3.png" alt="icono" class="icono">U3 and U1<img src="../assets/icon/G1.png" alt="icono" class="icono"></td>
-              <td>AJ Katzenmaier</td>
-              <td>9:30 a.m.</td>
-            </tr>
-            <tr>
-              <td><img src="../assets/icon/G5.png" alt="icono" class="icono">U5 and U6<img src="../assets/icon/G6.png" alt="icono" class="icono"></td>
-              <td>Howard A Yeager</td>
-              <td>1:00 p.m.</td>
-            </tr>
-          </table>
+            <TablaOct></TablaOct>
       </div>
     </div>
 
@@ -182,6 +74,9 @@
 </template>
 
 <script>
+import TablaSep from '../components/TablaSep.vue';
+import TablaOct from '../components/TablaOct.vue';
+
 export default {
   data () {
     return {
@@ -195,8 +90,12 @@ export default {
     handleVisibility (isVisible) {
       this.isVisible = isVisible
     }
+  },
+  name:'Info',
+  components:{
+    TablaSep,
+    TablaOct,
   }
-
 }
 </script>
 
