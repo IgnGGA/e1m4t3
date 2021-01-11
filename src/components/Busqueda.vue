db47f0e7263c4d15900cb19412d6883343478f60
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-11 11:01:33 -0300

    version prelimiar

---
+++ src/components/Busqueda.vue
@@ -0,0 +1,29 @@
+<template >
+  <div>
+    <b-col>
+      <div id="porMes">
+        <b-button @click="showS = !showS" class="mb-2" variant="success"
+          >September</b-button>&nbsp;
+        <b-button @click="showO = !showO" class="mb-2" variant="info"
+          >October</b-button>
+      </div>
+    </b-col>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showS: true,
+      showO: true,
+      isVisible: false,
+    };
+  },
+  methods: {
+    handleVisibility(isVisible) {
+      this.isVisible = isVisible;
+    },
+  },
+};
+</script>
