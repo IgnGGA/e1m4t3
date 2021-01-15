66d1103b801c4b8e52c166aadbd1d10dc91a59bc
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-15 12:50:19 -0300

    cambios menores

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -34,7 +34,7 @@ export default {
             const today=new Date();
             const date=today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
             const time=today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
-            const dataTime=date+''+time;
+            const dataTime=date+' - '+time;
             this.datatime=dataTime;
             const message={
                 text:this.showMessage,
